ab62c8c5860bdc831f069adb6bd7d8c12a4f5b28
Tomasz Jankowski <tomasz.jankowski@idea-inspired.com>
2020-05-19 15:00:14 +0200

    Add main application container with routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import Calendar from "./containers/Calendar";
+import Goals from "./containers/Goals";
+import Navigation from "./components/Navigation";
+import { Switch, Route } from "react-router-dom";
+import { StoreProvider } from "./store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function User() {
+  return <span>User</span>;
 }
 
-export default App;
+const Container = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+`;
+
+export default function App() {
+  return (
+    <StoreProvider>
+      <Navigation />
+      <Container>
+        <Switch>
+          {/* If the current URL is /about, this route is rendered
+              while the rest are ignored */}
+          <Route path="/user">
+            <User />
+          </Route>
+
+          {/* Note how these two routes are ordered. The more specific
+              path="/contact/:id" comes before path="/contact" so that
+              route will render when viewing an individual contact */}
+          <Route path="/goals">
+            <Goals />
+          </Route>
+
+          {/* If none of the previous routes render anything,
+              this route acts as a fallback.
+  
+              Important: A route with path="/" will *always* match
+              the URL because all URLs begin with a /. So that's
+              why we put this one last of all */}
+          <Route path="/">
+            <Calendar />
+          </Route>
+        </Switch>
+      </Container>
+    </StoreProvider>
+  );
+}
